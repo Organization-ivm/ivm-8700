ade01776476a7cf20dd6867461b6282b05628135
gitzsq <gitmima123>
2018-11-21 22:52:11 +0800

    add main view select

--- ivm-8700/app/src/main/java/com/ivms/ivms8700/view/MainActivity.java
+++ ivm-8700/app/src/main/java/com/ivms/ivms8700/view/MainActivity.java
@@ -1,11 +1,14 @@
 package com.ivms.ivms8700.view;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ivms.ivms8700.R;
 import com.ivms.ivms8700.view.fragment.ImageManagementFragment;
@@ -25,6 +28,15 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
       private ImageManagementFragment imFragment;
       private MessageFragment messageFragment;
       private MyFragment myFragment;
+    private ImageView image_management_img;
+    private TextView image_management_txt;
+    private ImageView message_img;
+    private TextView my_txt;
+    private TextView message_txt;
+    private ImageView my_img;
+    private ImageView video_img;
+    private TextView video_txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,27 +57,44 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         ll_message.setOnClickListener(this);
         ll_my.setOnClickListener(this);
 
+        video_img=(ImageView)findViewById(R.id.video_img);
+        video_txt=(TextView)findViewById(R.id.video_txt);
+        image_management_img =(ImageView)findViewById(R.id.image_management_img);
+        image_management_txt=(TextView)findViewById(R.id.image_management_txt);
+        message_img =(ImageView)findViewById(R.id.message_img);
+        message_txt=(TextView)findViewById(R.id.message_txt);
+        my_img =(ImageView)findViewById(R.id.my_img);
+        my_txt=(TextView)findViewById(R.id.my_txt);
+
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.video_lay:
+
+                updateUI(0);
                 initFragment(0);
                 break;
             case R.id.image_management_lay:
+                updateUI(1);
                 initFragment(1);
                 break;
             case R.id.message_lay:
+                updateUI(2);
                 initFragment(2);
                 break;
             case R.id.my_lay:
+                updateUI(3);
                 initFragment(3);
                 break;
         }
     }
 
-          private void initFragment(int index) {
+
+
+    private void initFragment(int index) {
                  // 由于是引用了V4包下的Fragment，所以这里的管理器要用getSupportFragmentManager获取
                  FragmentManager fragmentManager = getSupportFragmentManager();
                  // 开启事务
@@ -134,4 +163,54 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                      }
 
              }
+
+    //更新底部按钮状态
+    @SuppressLint("ResourceAsColor")
+    private void updateUI(final int i) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+        if(i==0){
+            video_img.setBackgroundResource(R.drawable.xuanzhong);
+            video_txt.setTextColor((getResources().getColor(R.color.text_select_color)));
+            image_management_img.setBackgroundResource(R.drawable.tuxiangguanli);
+            image_management_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            message_img.setBackgroundResource(R.drawable.xiaoxi_1);
+            message_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            my_img.setBackgroundResource(R.drawable.wode);
+            my_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+        }
+        if(i==1){
+            video_img.setBackgroundResource(R.drawable.shi);
+            video_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            image_management_img.setBackgroundResource(R.drawable.tuxiangguanli_1);
+            image_management_txt.setTextColor((getResources().getColor(R.color.text_select_color)));
+            message_img.setBackgroundResource(R.drawable.xiaoxi_1);
+            message_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            my_img.setBackgroundResource(R.drawable.wode);
+            my_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+        }
+        if(i==2){
+            video_img.setBackgroundResource(R.drawable.shi);
+            video_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            image_management_img.setBackgroundResource(R.drawable.tuxiangguanli);
+            image_management_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            message_img.setBackgroundResource(R.drawable.xiaoxi);
+            message_txt.setTextColor((getResources().getColor(R.color.text_select_color)));
+            my_img.setBackgroundResource(R.drawable.wode);
+            my_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+        }
+        if(i==3){
+            video_img.setBackgroundResource(R.drawable.shi);
+            video_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            image_management_img.setBackgroundResource(R.drawable.tuxiangguanli);
+            image_management_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            message_img.setBackgroundResource(R.drawable.xiaoxi_1);
+            message_txt.setTextColor((getResources().getColor(R.color.text_noselect_color)));
+            my_img.setBackgroundResource(R.drawable.wode_1);
+            my_txt.setTextColor((getResources().getColor(R.color.text_select_color)));
+        }
+            }
+        });
+    }
 }
